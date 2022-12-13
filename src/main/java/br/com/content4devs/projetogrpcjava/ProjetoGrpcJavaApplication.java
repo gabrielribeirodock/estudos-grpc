f511b1c49b7f7e4f71a74ce50ac1c10255edc2ad
Gabriel Ribeiro <gabriel.bezerra@dock.tech>
2022-12-13 17:09:44 -0300

    Setup inicial e serviço Hello World

---
+++ src/main/java/br/com/content4devs/projetogrpcjava/ProjetoGrpcJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.content4devs.projetogrpcjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGrpcJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGrpcJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/content4devs/projetogrpcjava/ProjetoGrpcJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.content4devs.projetogrpcjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGrpcJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
