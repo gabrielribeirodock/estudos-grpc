e9cb1abc90f859113baca4aa89ad7407017b3051
Gabriel Ribeiro <gabriel.bezerra@dock.tech>
2022-12-15 14:03:49 -0300

    Implementando teste para método create

---
+++ src/test/java/br/com/content4devs/projetogrpcjava/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,44 @@
+package br.com.content4devs.projetogrpcjava.service.impl;
+
+import br.com.content4devs.projetogrpcjava.domain.Product;
+import br.com.content4devs.projetogrpcjava.dto.ProductInputDTO;
+import br.com.content4devs.projetogrpcjava.dto.ProductOutputDTO;
+import br.com.content4devs.projetogrpcjava.repository.ProductRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceImplTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    @Test
+    @DisplayName("When create product is called with valid data a product is returned")
+    public void createProductSuccessTest(){
+        Product product = new Product(1L, "product name", 10.0, 10);
+
+        Mockito.when(productRepository.save(Mockito.any())).thenReturn(product);
+
+        ProductInputDTO productInputDTO = new ProductInputDTO(
+                product.getName(),
+                product.getPrice(),
+                product.getQuantityInStock()
+        );
+
+        ProductOutputDTO productOutputDTO = productService.create(productInputDTO);
+
+        Assertions.assertThat(productOutputDTO)
+                .usingRecursiveComparison()
+                .isEqualTo(product);
+    }
+}
