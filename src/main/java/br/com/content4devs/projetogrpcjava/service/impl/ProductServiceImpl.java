63de63c92fc04899432bba4d8389f489b8d2efd5
Gabriel Ribeiro <gabriel.bezerra@dock.tech>
2022-12-19 11:10:57 -0300

    Criando validação de nomes duplicados e ExceptionHandler

--- src/main/java/br/com/content4devs/projetogrpcjava/service/impl/ProductServiceImpl.java
+++ src/main/java/br/com/content4devs/projetogrpcjava/service/impl/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package br.com.content4devs.projetogrpcjava.service.impl;
 
 import br.com.content4devs.projetogrpcjava.dto.ProductInputDTO;
 import br.com.content4devs.projetogrpcjava.dto.ProductOutputDTO;
+import br.com.content4devs.projetogrpcjava.exceptions.ProductAlreadyExistsException;
 import br.com.content4devs.projetogrpcjava.repository.ProductRepository;
 import br.com.content4devs.projetogrpcjava.service.IProductService;
 import br.com.content4devs.projetogrpcjava.util.ProductConverterUtil;
@@ -18,6 +19,7 @@ public class ProductServiceImpl implements IProductService {
 
     @Override
     public ProductOutputDTO create(ProductInputDTO inputDTO) {
+        checkDuplicity(inputDTO.getName());
         var product = ProductConverterUtil.productInputDtoToProduct(inputDTO);
         var productCreated = productRepository.save(product);
         return ProductConverterUtil.productToProductOutputDto(productCreated);
@@ -36,5 +38,12 @@ public class ProductServiceImpl implements IProductService {
     @Override
     public void delete(Long id) {
 
+    }
+
+    private void checkDuplicity(String name){
+        productRepository.findByNameIgnoreCase(name)
+                .ifPresent(e -> {
+                    throw new ProductAlreadyExistsException(name);
+                });
     }
 }
