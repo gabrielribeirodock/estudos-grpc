3d3b8de2b54111c61e8a306e4ad9e82befad63c3
Gabriel Ribeiro <gabriel.bezerra@dock.tech>
2022-12-20 10:12:12 -0300

    Implementando teste integrado para o cenário de exceção

--- src/test/java/br/com/content4devs/projetogrpcjava/resource/ProductResourceIntegrationTest.java
+++ src/test/java/br/com/content4devs/projetogrpcjava/resource/ProductResourceIntegrationTest.java
@@ -3,10 +3,14 @@ package br.com.content4devs.projetogrpcjava.resource;
 import br.com.content4devs.ProductRequest;
 import br.com.content4devs.ProductResponse;
 import br.com.content4devs.ProductServiceGrpc;
+import io.grpc.StatusRuntimeException;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.assertj.core.api.Assertions;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
@@ -19,11 +23,20 @@ public class ProductResourceIntegrationTest {
     @GrpcClient("inProcess")
     private ProductServiceGrpc.ProductServiceBlockingStub serviceBlockingStub;
 
+    @Autowired
+    private Flyway flyway;
+
+    @BeforeEach
+    public void setUp(){
+        flyway.clean();
+        flyway.migrate();
+    }
+
     @Test
     @DisplayName("When valid data is provided a product is created")
     public void createProductSuccessTest(){
         ProductRequest productRequest = ProductRequest.newBuilder()
-                        .setName("Product name")
+                        .setName("product name")
                         .setPrice(10.00)
                         .setQuantityInStock(100).build();
 
@@ -33,5 +46,20 @@ public class ProductResourceIntegrationTest {
                 .usingRecursiveComparison()
                 .comparingOnlyFields("name", "price", "quantity_in_stock")
                 .isEqualTo(productResponse);
+    }
+
+    @Test
+    @DisplayName("When create is called with duplicated name, throw ProductAlreadyExistsException")
+    public void createProductAlreadyExistsExceptionTest(){
+        ProductRequest productRequest = ProductRequest.newBuilder()
+                .setName("Product A")
+                .setPrice(10.00)
+                .setQuantityInStock(100)
+                .build();
+
+        Assertions.assertThatExceptionOfType(StatusRuntimeException.class)
+                .isThrownBy(() -> serviceBlockingStub.create(productRequest))
+                .withMessage("ALREADY_EXISTS: Produto Product A já cadastrado no sistema.");
+
     }
 }
